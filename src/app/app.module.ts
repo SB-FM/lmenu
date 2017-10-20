c0e0dbb0a1ed0d4736263fbd3b39e5d024eafd64
NiL <superbeast@email.com>
2017-10-20 02:37:07 +0200

    added MenuItem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LeftMenuComponent } from './left-menu/left-menu.component';
+import { LeftMenuItemComponent } from './left-menu-item/left-menu-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftMenuComponent,
+    LeftMenuItemComponent
   ],
   imports: [
     BrowserModule
